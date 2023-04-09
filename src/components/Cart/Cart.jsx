5261d1cf6c1330c926cf5696f614a3f97d64fcb1
showvike <showvike@gmail.com>
2023-04-10 00:46:23 +0600

    conditional rendering

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
+  let message;
+  if (cart.length === 0) message = <p>Please Order</p>;
+  else message = <p>Thank You</p>;
+
   return (
-    <div className="cart">
+    <div className={`cart ${cart.length > 6 ? "blue" : ""}`}>
       <h3>Order Summary: {cart.length}</h3>
-      <ol>
+      {message}
+      <ol className={cart.length < 3 ? "red" : "green"}>
         {cart.map((tShirt) => (
           <li key={tShirt._id}>
             {tShirt.name}
@@ -13,6 +18,9 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
           </li>
         ))}
       </ol>
+      {cart.length <= 2 ? <p>Fokirrr</p> : <p>Aro Order Den</p>}
+      {cart.length === 3 && <p>Tin ta hosie</p>}
+      {cart.length <= 3 || <p>Boroloxxx</p>}
     </div>
   );
 };
